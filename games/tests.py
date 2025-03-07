9db171166ac73a202a54660f9747ab6d5f368b56
AsaevichNikita <nasaevich459@gmail.com>
2025-03-07 22:25:01 +0600

    add swagger

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from factory import DjangoModelFactory, Faker
+
+from models import GoGame
+
+
+class CompanyFactory(DjangoModelFactory):
+    name = Faker('company')
+    description = Faker('text')
+    website = Faker('url')
+    street_line_1 = Faker('street_address')
+    city = Faker('city')
+    state = Faker('state_abbr')
+    zipcode = Faker('zipcode')
+
+    class Meta:
+        model = Company
